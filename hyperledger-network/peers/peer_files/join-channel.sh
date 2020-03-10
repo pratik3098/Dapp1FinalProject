1e0873cc5ad775e0bc429bb0b17b78721c6354ae
pratik3098 <pratik3098@gmail.com>
2020-03-09 20:51:49 -0400

    network working/ proposal failure

--- hyperledger-network/peers/peer_files/join-channel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -f ./foodsupplychannel_0.block
-rm -f ./foodsupplychannel.block
-peer channel fetch 0  -o $ORDERER_ADDRESS -c foodsupplychannel
-
-# Join the channel
-peer channel join -b $FOODSUPPLY_CHANNEL_BLOCK_0 -o $ORDERER_ADDRESS
-
