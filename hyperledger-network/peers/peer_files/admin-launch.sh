5fc17ed2d91c2a6a6d5947b710b2a406a7738c5d
pratik3098 <pratik3098@gmail.com>
2020-03-10 01:02:59 -0400

    peer script updated

--- hyperledger-network/peers/peer_files/admin-launch.sh
+++ hyperledger-network/peers/peer_files/admin-launch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 peer channel create -o $ORDERER_ADDRESS -c foodsupplychannel -f $CHANNEL_TX_FILE
-
+sleep 3
 peer node status
 
 
 peer channel fetch 0  -o $ORDERER_ADDRESS -c foodsupplychannel
 
-
+sleep 3
 #2 Join the channel
 peer channel join -b $FOODSUPPLY_CHANNEL_BLOCK_0 -o $ORDERER_ADDRESS
 
--- hyperledger-network/peers/peer_files/launch-peer.sh
+++ hyperledger-network/peers/peer_files/launch-peer.sh
@@ -6,7 +6,7 @@ peer channel fetch 0  -o $ORDERER_ADDRESS -c foodsupplychannel
 
 
 #2 Join the channel
-peer channel join -b $FOODSUPPLY_CHANNEL_BLOCK_0 -o $ORDERER_ADDRESS
+peer channel join -b $FOODSUPPLY_CHANNEL_BLOCK -o $ORDERER_ADDRESS
 
 #./chain-test.sh install 
 
