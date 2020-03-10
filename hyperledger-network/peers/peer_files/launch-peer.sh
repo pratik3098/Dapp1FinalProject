0689f79cd01a5b0cc423c389a4e08fe9d09f2a06
pratik3098 <pratik3098@gmail.com>
2020-03-09 20:27:01 -0400

    network working/ proposal fail

--- hyperledger-network/peers/peer_files/launch-peer.sh
+++ hyperledger-network/peers/peer_files/launch-peer.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 peer node status
 
-export CHANNEL_TX_FILE=./foodsupply-channel.tx
-
-# Execute the channel create command
-peer channel create -o $ORDERER_ADDRESS -c foodsupplychannel -f $CHANNEL_TX_FILE
-
-rm -f foodsupplychannel_0.block
-rm -f foodsupplychannel.block
-
-export FOODSUPPLY_CHANNEL_BLOCK_0=./commercialpaperchannel_0.block
 
 peer channel fetch 0  -o $ORDERER_ADDRESS -c foodsupplychannel
+
 
 #2 Join the channel
 peer channel join -b $FOODSUPPLY_CHANNEL_BLOCK_0 -o $ORDERER_ADDRESS
